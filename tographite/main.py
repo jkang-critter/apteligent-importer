43cc469dc02edbcc4e9d0205f373dc29f7b19d8d
Paul Frederiks <pfrederiks@ebay.com>
2016-03-31 17:27:02 +0200

    refactoring of tographite/main.py

--- tographite/main.py
+++ tographite/main.py
@@ -4,26 +4,34 @@ import logging
 import pickle
 import struct
 import socket
-import pprint
 from collections import deque
 
 log = logging.getLogger(__name__)
 
-# Special characters are not supported for use in graphite expressions.
+# Some special characters are not supported for use in graphite expressions.
+# Regardless, whisper will happily create a file with names containing
+# those characters.
 # delchars contains all the characters to remove from a metric path
+# that are problematic.
 delchars = '(){}[]!"#$%&\'*+/<=>?@\\^`|~\t\n\r\x0b\x0c'
 # Dots create a new directory in whisperdb. They are replaced with a dash.
 # Spaces, commas, parentheses are problematic, we replace them with underscores
 inchars = '. ,/:;'
 ouchars = '-_____'
 
-
+# Here I avoid the use of maketrans, because of python2 / python3 differences
+# for unicode a transtable is just a map of ordinals.
 transtable = {ord(a): ord(b) for a, b in zip(inchars, ouchars)}
 deltable = {ord(d): None for d in delchars}
 transtable.update(deltable)
 
 
 def sanitize(metric):
+    """
+    Sanitize the input metric paths and accepts lists or dot seperated strings.
+    Returns a dot separated string in unicode/ python 3 string. Bytes or python2
+    strings are decoded to unicode.
+    """
 
     if type(metric) == list:
             l = list()
@@ -40,6 +48,33 @@ def sanitize(metric):
             metric = metric.decode().translate(deltable)
 
     return metric
+
+
+def plainmessage(metrics):
+    """
+    Return message conforming to the graphite line protocol
+    """
+    def processtuple(tpl):
+        metric, (timestamp, value) = tpl
+        return '{metric} {value:g} {timestamp:.3f}'.format(**locals())
+
+    return '\n'.join([processtuple(tpl) for tpl in metrics])
+
+
+def picklemessage(metrics):
+    """
+    Return message conforming to the graphite pickle protocol
+    """
+    def processtuple(tpl):
+        metric, (timestamp, value) = tpl
+        return metric.encode('utf-8'), (float(timestamp), float(value))
+
+    lst = [processtuple(tpl) for tpl in metrics]
+
+    # Use pickle protocol 2 as carbon is python2 only (march 2016)
+    payload = pickle.dumps(lst, 2)
+    header = struct.pack("!L", len(payload))
+    return header + payload
 
 
 class CarbonSink(object):
@@ -70,17 +105,20 @@ class CarbonSink(object):
         self.connection = (host, port)
         self.protocol = protocol
         self.max_buffer = max_buffer
+        # All metrics are buffered in a deque. This is a thread safe and more
+        # performant datastructure than using a plain list.
         self.buff = deque()
 
     def submit(self, metric, value, timestamp):
         """
-        Add a tuple in the form (metric, (timestamp, value)) to the list
+        Add a tuple in the form (metric, (timestamp, value)) to the deque
         self.buff
         When max_buffer is reached, flush the buffer to graphite.
         Arguments:
         - metric is either a dotted string or a list representing the metric.
-        - value is a number representing the value.
-        - timestamp is a unix timestamps in seconds since epoch.
+        - value is a number representing the value. Will be cast to float.
+        - timestamp is a unix timestamps in seconds since epoch. Will be cast
+        to float.
         """
         metric = sanitize(metric)
         message = metric, (timestamp, value)
@@ -114,17 +152,13 @@ class CarbonSink(object):
             return
 
         if self.protocol == 'pickle':
-            # Use pickle protocol 2 as carbon is python2 only (march 2016)
-            payload = pickle.dumps(lst, 2)
-            header = struct.pack("!L", len(payload))
-            message = header + payload
-        elif self.protocol == 'plain' or self.protocol == 'dummy':
-            message = '\n'.join(
-                ('{0[0]} {0[1][1]} {0[1][0]}'.format(metric)
-                    for metric in lst))
-            if self.protocol == 'dummy':
-                log.info(message)
-                return
+            message = picklemessage(lst)
+        elif self.protocol == 'plain':
+            message = plainmessage(lst).encode('utf-8')
+        elif self.protocol == 'dummy':
+            message = plainmessage(lst)
+            log.info('Dummy protocol:\nSTARTDATA\n%s\nENDDATA', message)
+            return
         else:
             log.warning('Graphite protocol unknown: %s', self.protocol)
             raise ValueError('Graphite protocol unknown')
